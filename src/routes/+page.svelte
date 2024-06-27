0a4017c910e7880c5f8fa3daaaf64444ac8159c5
selectdev <selectdev01@gmail.com>
2024-06-27 05:10:47 +0000

    Added Testimonials

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -105,15 +105,36 @@
 		Case: 'Corsair iCUE 4000X RGB',
 		Storage: 'SAMSUNG 980 PRO (SSD) - 2TB',
 		Motherboard: 'MSI MAG B550 Tomahawk MAX WiFi',
-		'Graphics Card': 'GeForce RTX 4060',
+		//'Graphics Card': 'GeForce RTX 4060',
 		CPU: 'AMD Ryzen 7 5700G',
-		'CPU Cooler': 'Corsair iCUE H100i Elite LCD',
+		//'CPU Cooler': 'Corsair iCUE H100i Elite LCD',
 		'Power Supply': 'Corsair RM750x',
 		RAM: 'Corsair VENGEANCE RGB PRO (DDR4) - 16GB'
 	};
+
+	const testimonials: {
+		name: string;
+		description: string;
+		project: string;
+		link: string;
+		image: string;
+	}[] = [
+		{
+			name: 'Emil Ilieff',
+			description:
+				"I highly recommend Select, whose skills and dedication were crucial to our project's success. As a full stack developer, his expertise in both front-end and back-end development consistently delivered high-quality solutions. Select's problem-solving ability, proficiency in various programming languages, and exceptional collaboration skills ensured thorough execution and innovation. His clear communication and strategic mindset were invaluable. I am confident in his continued success in future projects.",
+			project: 'AntiRaid',
+			link: 'https://antiraid.xyz/',
+			image: 'https://popkat.purrquinox.com/939058556a61a4efe6e07243c6b1732a_588'
+		}
+	];
 </script>
 
-<Meta />
+<Meta
+	Description="I'm a {age}-year-old full-stack developer with six years of experience, working with a variety of programming languages and technologies. I'm passionate about technology and excited to start a new project. {available
+		? "Right now, I'm available for hire and ready to bring fresh ideas to your team."
+		: ''}"
+/>
 
 <main class="p-6">
 	<!-- About Me -->
@@ -139,7 +160,7 @@
 		</p>
 	</div>
 
-	<div class="p-5" />
+	<div class="p-3" />
 
 	<!-- Projects -->
 	<div id="projects">
@@ -152,12 +173,11 @@
 			{#each projects as project}
 				<div class="self-auto">
 					<div
-						class="block max-w-sm p-3 border rounded-br-lg hover:rounded-tl-lg shadow bg-surface-700 border-surface-800 hover:bg-surface-600"
+						class="block max-w-sm p-3 border rounded-md shadow bg-surface-700 border-surface-800 hover:bg-surface-600"
 					>
 						<a href={project.link}>
 							{#if project.flair}
-								<span
-									class="bg-surface-400 text-white p-1 text-xs px-1.5 rounded-l-md rounded-r-full font-bold"
+								<span class="bg-surface-400 text-white p-1 text-xs px-1.5 rounded-r-full font-bold"
 									>{project.flair}</span
 								>
 							{/if}
@@ -183,64 +203,116 @@
 				</div>
 			{/each}
 		</div>
+	</div>
 
-		<div class="p-5" />
+	<div class="p-3" />
 
-		<!-- Tech Stack -->
-		<div id="tech-stack">
-			<h1 class="text-primary-600 text-3xl font-bold tracking-tight">Tech Stack</h1>
-			<p class="text-primary-300 font-bold tracking-tight">
-				Oh, hello there. This is the stack, that i use when working on projects.
-			</p>
+	<!-- Tech Stack -->
+	<div id="tech-stack">
+		<h1 class="text-primary-600 text-3xl font-bold tracking-tight">Tech Stack</h1>
+		<p class="text-primary-300 font-bold tracking-tight">
+			Oh, hello there. This is the stack, that i use when working on projects.
+		</p>
 
-			<div class="mt-2 grid grid-cols-1 gap-2 md:grid-cols-2 lg:grid-cols-3">
-				{#each stack as p}
-					<div class="self-auto">
-						<div
-							class="block max-w-sm p-3 border rounded-br-lg hover:rounded-tl-lg shadow bg-surface-700 border-surface-800 hover:bg-surface-600"
-						>
-							<a href={p.link}>
-								<div class="flex">
-									{#if p.image.startsWith('https://')}
-										<img
-											src={p.image}
-											class="h-[30px] bg-surface-700 rounded-full"
-											height="30px"
-											alt="{p.name} logo"
-										/>
-									{:else}
-										<i class="mt-3 {p.image} fa-xl max-h-[30px] text-white rounded-full" />
-									{/if}
+		<div class="mt-2 grid grid-cols-1 gap-2 md:grid-cols-2 lg:grid-cols-3">
+			{#each stack as p}
+				<div class="self-auto">
+					<div
+						class="block max-w-sm p-3 border rounded-md shadow bg-surface-700 border-surface-800 hover:bg-surface-600"
+					>
+						<a href={p.link}>
+							<div class="flex">
+								{#if p.image.startsWith('https://')}
+									<img
+										src={p.image}
+										class="h-[30px] bg-surface-700 rounded-full"
+										height="30px"
+										alt="{p.name} logo"
+									/>
+								{:else}
+									<i class="mt-3 {p.image} fa-xl max-h-[30px] text-white rounded-full" />
+								{/if}
 
-									<h5
-										class="ml-2 text-lg font-bold overflow-x-auto tracking-tight text-secondary-400"
-									>
-										{p.name}
-									</h5>
-								</div>
-								<p class="mt-2 text-base font-semibold text-secondary-500">{p.description}</p>
-							</a>
-						</div>
+								<h5
+									class="ml-2 text-lg font-bold overflow-x-auto tracking-tight text-secondary-400"
+								>
+									{p.name}
+								</h5>
+							</div>
+
+							<p class="mt-2 text-base font-semibold text-secondary-500">{p.description}</p>
+						</a>
 					</div>
-				{/each}
-			</div>
-		</div>
-
-		<div class="p-5" />
-
-		<!-- PC Specs -->
-		<div id="pc-specs">
-			<h1 class="text-primary-600 text-3xl font-bold tracking-tight">PC Specs</h1>
-			<p class="text-primary-300 font-bold tracking-tight">Here are some of the specs of my PC.</p>
-
-			<div class="mt-2" />
-
-			{#each Object.entries(specs) as spec}
-				<p class="ml-3 pt-1 text-primary-400 font-bold italic text-md">
-					- <span class="text-primary-500 tracking-tight">{spec[0]}:</span>
-					{spec[1]}
-				</p>
+				</div>
 			{/each}
 		</div>
+	</div>
+
+	<div class="p-3" />
+
+	<!-- Testimonials -->
+	<div id="testimonials">
+		<h1 class="text-primary-600 text-3xl font-bold tracking-tight">Testimonials</h1>
+		<p class="text-primary-300 font-bold tracking-tight">
+			Here are some testimonials provided by some of my clients!
+		</p>
+
+		<div class="mt-2 grid grid-cols-1 gap-2 md:grid-cols-2 lg:grid-cols-3">
+			{#each testimonials as testimonial}
+				<div class="self-auto">
+					<div
+						class="block max-w-sm p-3 border rounded-md shadow bg-surface-700 border-surface-800 hover:bg-surface-600"
+					>
+						<a href={testimonial.link}>
+							<span class="bg-surface-400 text-white p-1 text-xs px-1.5 rounded-r-full font-bold"
+								>VERIFIED</span
+							>
+
+							<div class="mt-3 flex">
+								<img
+									src={testimonial.image}
+									class="h-[30px] bg-surface-700 rounded-full"
+									height="30px"
+									alt="{testimonial.name} logo"
+								/>
+
+								<h5
+									class="ml-2 text-lg font-bold overflow-x-auto tracking-tight text-secondary-400"
+								>
+									{testimonial.name} <span class="opacity-60">(with {testimonial.project})</span>
+								</h5>
+							</div>
+
+							<p class="mt-2 text-sm lg:text-base font-semibold text-secondary-500">
+								{testimonial.description}
+							</p>
+						</a>
+					</div>
+				</div>
+			{/each}
+		</div>
+	</div>
+
+	<div class="p-3" />
+
+	<!-- PC Specs -->
+	<div id="pc-specs">
+		<h1 class="text-primary-600 text-3xl font-bold tracking-tight">PC Specs</h1>
+		<p class="text-primary-300 font-bold tracking-tight">
+			Here are some of the specifications of my PC.
+		</p>
+
+		<div class="mt-2" />
+
+		<ol class="ml-6 list-disc">
+			{#each Object.entries(specs) as spec}
+				<li class="ml-3 pt-1 text-primary-400 font-bold italic text-md">
+					<span class="text-primary-500 tracking-tight">{spec[0]}:</span>
+					{spec[1]}
+				</li>
+			{/each}
+		</ol>
+
+		<p class="text-primary-300 font-bold tracking-tight">* Specifications are prone to change.</p>
 	</div>
 </main>
