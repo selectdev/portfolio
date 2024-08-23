83918c8b0d401ddd2836b728ebbd39f6eb36e070
Select <66577268+selectdev@users.noreply.github.com>
2024-08-23 14:50:36 -0500

    Update +server.ts

--- src/routes/api/data/+server.ts
+++ src/routes/api/data/+server.ts
@@ -3,7 +3,7 @@ import { load as loadLayoutData } from '../../+layout.server';
 export async function GET() {
     const data = await loadLayoutData();
 
-    return new Response(JSON.stringify(data),
+    return new Response(JSON.stringify(data).trim(),
         {
             headers: {
                 'Content-Type': 'application/json'
